67ed53ac1ada6fa5d39fd37309acbeba3a8a22ce
IvanAndriWahyudi <ivanandri.w04@gmail.com>
2024-11-24 16:26:18 +0700

    njk

--- script/script.js
+++ script/script.js
@@ -10,6 +10,7 @@ const paper8 = document.querySelector("#p8");
 const paper9 = document.querySelector("#p9");
 
 const gkanan = document.querySelector("#gkanan");
+const heartContainer = document.getElementById("heart");
 
 const b8 = document.querySelector(".b8");
 const b1 = document.querySelector(".b1");
@@ -33,32 +34,31 @@ window.addEventListener("mousedown", () => {
 // Daftar waktu tertentu (dalam detik) dan perintah yang akan dijalankan
 const events = [
   {
-    time: 1,
+    time: 1000,
     action: () => {
       centerBook();
       paper1.classList.add("flipped");
       paper1.style.zIndex = 9;
       b1.style.boxShadow = "-1px 10px 10px -2px rgba(0, 0, 0, 0.75)";
-      const text = "Katakan Cinta";
-      // const text2 = "&#9829";
-      animasiTulisan(text, b1Content);
+      const text = "Katakan Cinta ";
+      animasiTulisan2(text, b1Content);
     },
   },
   {
-    time: 3,
+    time: 4000,
     action: () => {
       displayText(b1Content);
     },
   },
   {
-    time: 4,
+    time: 4500,
     action: () => {
-      const text = "Katakan Cinta";
-      animasiTulisan(text, b1Content);
+      const text = "Katakan Cinta ";
+      animasiTulisan2(text, b1Content);
     },
   },
   {
-    time: 6,
+    time: 6000,
     action: () => {
       paper2.classList.add("flipped");
       paper1.style.zIndex = 1;
@@ -68,17 +68,17 @@ const events = [
     },
   },
   {
-    time: 10,
+    time: 10000,
     action: () => {
       paper3.classList.add("flipped");
       paper2.style.zIndex = 2;
       paper3.style.zIndex = 9;
       const text = "Kau lebih dari teman berbagi";
-      animasiTulisan(text, b3Content);
+      animasiTulisanResize(text, b3Content);
     },
   },
   {
-    time: 15,
+    time: 15000,
     action: () => {
       paper4.classList.add("flipped");
       paper3.style.zIndex = 3;
@@ -88,17 +88,17 @@ const events = [
     },
   },
   {
-    time: 18,
+    time: 18000,
     action: () => {
       paper5.classList.add("flipped");
       paper4.style.zIndex = 4;
       paper5.style.zIndex = 9;
       const text = "Cinta katakan cinta";
-      animasiTulisan(text, b5Content);
+      animasiTulisan2(text, b5Content);
     },
   },
   {
-    time: 22,
+    time: 22000,
     action: () => {
       paper6.classList.add("flipped");
       paper5.style.zIndex = 5;
@@ -108,17 +108,17 @@ const events = [
     },
   },
   {
-    time: 26,
+    time: 26000,
     action: () => {
       paper7.classList.add("flipped");
       paper6.style.zIndex = 6;
       paper7.style.zIndex = 9;
       const text = "Kau lebih dari teman berbagi";
-      animasiTulisan(text, b7Content);
+      animasiTulisanResize(text, b7Content);
     },
   },
   {
-    time: 30,
+    time: 30000,
     action: () => {
       paper8.classList.add("flipped");
       paper7.style.zIndex = 7;
@@ -134,17 +134,44 @@ const events = [
   },
 ];
 
-//function animasi tulisan
 function animasiTulisan(text, content) {
   text.split("").forEach((char, index) => {
     const span = document.createElement("span");
     span.textContent = char === " " ? "\u00A0" : char;
+    span.classList.add("font-size-span");
     span.style.animationDelay = `${index * 0.1}s`; // Jeda antar huruf
     content.appendChild(span);
   });
 }
 
-//function animasi keluar dua kali
+function animasiTulisanResize(text, content) {
+  text.split("").forEach((char, index) => {
+    const span = document.createElement("span");
+    span.textContent = char === " " ? "\u00A0" : char;
+    span.classList.add("font-size-span2");
+    span.style.animationDelay = `${index * 0.1}s`; // Jeda antar huruf
+    content.appendChild(span);
+  });
+}
+
+//function animasi tulisan
+function animasiTulisan2(text, content) {
+  text.split("").forEach((char, index) => {
+    const span = document.createElement("span");
+    span.textContent = char === " " ? "\u00A0" : char;
+    span.classList.add("font-size-span");
+    span.style.animationDelay = `${index * 0.1}s`; // Jeda antar huruf
+    content.appendChild(span);
+  });
+
+  const heartIcon = document.createElement("span");
+  heartIcon.textContent = " ♥";
+  heartIcon.classList.add("font-size-span");
+  heartIcon.style.animationDelay = `1.3s`;
+  content.appendChild(heartIcon);
+}
+
+//hapus span
 function displayText(content) {
   const spans = content.querySelectorAll("span"); // Ambil semua elemen <span> dalam container
   spans.forEach(span => {
@@ -157,15 +184,12 @@ let processedTimes = new Set();
 
 // Event listener untuk memeriksa waktu audio
 audio.addEventListener("timeupdate", () => {
-  const currentTime = audio.currentTime; // Ambil waktu saat ini dalam detik (termasuk desimal)
+  const currentTimeMs = Math.floor(audio.currentTime * 1000); // Waktu dalam milidetik
 
   // Loop melalui daftar events
   events.forEach(event => {
     // Jika currentTime lebih besar atau sama dengan waktu event dan event belum diproses
-    if (
-      Math.abs(currentTime - event.time) < 0.1 &&
-      !processedTimes.has(event.time)
-    ) {
+    if (currentTimeMs >= event.time && !processedTimes.has(event.time)) {
       event.action(); // Jalankan perintah
       processedTimes.add(event.time); // Tandai waktu sebagai sudah diproses
     }
